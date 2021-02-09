66376a238a7b1a6dff707d6824e54ea409966e3a
kao <kmc558821@gmail.com>
2021-02-09 23:20:21 +0800

    emotion

--- static/script.js
+++ static/script.js
@@ -3,10 +3,10 @@ const video = document.getElementById('video')
 var tempemotion = 'non';
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('../models')
 ]).then(startVideo)
 
 function startVideo() {
