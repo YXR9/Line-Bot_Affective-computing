ef760f00e7735fa2689ef403dad42422d34326c8
kao <kmc558821@gmail.com>
2021-02-09 22:35:20 +0800

    edit

--- app.py
+++ app.py
@@ -69,24 +69,6 @@ def pattern_mega(text):
         if re.search(pattern, text, re.IGNORECASE):
             return True
 
-
-def eyny_movie():
-    target_url = 'http://www.eyny.com/forum-205-1.html'
-    print('Start parsing eynyMovie....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ''
-    for titleURL in soup.select('.bm_c tbody .xst'):
-        if pattern_mega(titleURL.text):
-            title = titleURL.text
-            if '11379780-1-3' in titleURL['href']:
-                continue
-            link = 'http://www.eyny.com/' + titleURL['href']
-            data = '{}\n{}\n\n'.format(title, link)
-            content += data
-    return content
-
 def sum(a,b):
     return (a+b)
 
