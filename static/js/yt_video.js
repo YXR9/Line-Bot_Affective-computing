8c837aeafb37f813b257c8003bfa4c151d54bf55
kao <kmc558821@gmail.com>
2021-02-10 23:11:37 +0800

    camera

--- static/js/yt_video.js
+++ static/js/yt_video.js
@@ -1,17 +1,17 @@
-var tag = document.createElement('script');
-tag.id = 'iframe-demo';
-tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var tag = document.createElement('script');
+// tag.src = 'https://www.youtube.com/iframe_api';
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('course_video');
+    console.log("reday");
 }
 
 var myVar = setInterval(get_time, 5000);
 
 function get_time() {
     var time = player.getCurrentTime();
-    console.log(time);
+    console.log(time);s
 }
